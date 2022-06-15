a6015c6cfec5915073d2fb0c368b3491c718d100
unknown <lfcs18ts@gmail.com>
2022-06-15 20:45:04 -0300

    Inserindo axios e integração a api. Realizando testes para retorno dos dados, ainda de forma geral, sem a filtragem da pesquisa

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,6 +1,6 @@
-import Axios, { AxiosInstance } from 'axios';
+//import axios, { axiosInstance } from 'axios';
 
-export class baseApi {
+/*export class baseApi {
 
     BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=search+terms";
     protected api: AxiosInstance;
@@ -12,4 +12,26 @@ export class baseApi {
             timeout: 3000
         })
     }
+}*/
+
+/*import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'https://www.googleapis.com/books/v1/volumes?q=search+terms'
+});
+
+export const api = {
+    getAllPosts: async () => {
+        let response = await axios.get(`/posts`);
+        return response.data;
+    }
+}*/
+
+export const api = {
+    getAllLivros: async () => {
+        let response = await fetch('https://www.googleapis.com/books/v1/volumes?q=search+terms');
+        let json = await response.json();
+        return json;
+    }
 }
+
