5c0f9eb7dc35996f6958b95cdfb8786ff23b745a
unknown <lfcs18ts@gmail.com>
2022-06-19 20:43:21 -0300

    Inserindo o debug

--- src/Home.spec.tsx
+++ src/Home.spec.tsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { debug } from 'console';
 import { Home } from './components/Home/Home';
 
 describe('Home Component', () => {
@@ -8,7 +9,11 @@ describe('Home Component', () => {
 
         const searchButton = getByText('Carregar');
 
+        debug()
+
         userEvent.click(searchButton);
+
+        debug()
 
         expect(getByText('Novo')).toBeInTheDocument()
     })
