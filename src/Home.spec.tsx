2223a9c343117f79aa29f13e4adbbda3798539b4
unknown <lfcs18ts@gmail.com>
2022-06-19 20:42:22 -0300

    Alterando a função de teste

--- src/Home.spec.tsx
+++ src/Home.spec.tsx
@@ -1,11 +1,16 @@
-import { getByText, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Home } from './components/Home/Home';
 
 describe('Home Component', () => {
     it('must be able to search for books', () => {
-        const {getByText} = render(<Home />)
-    
+        const { getByText } = render(<Home />)
+
         const searchButton = getByText('Carregar');
+
+        userEvent.click(searchButton);
+
+        expect(getByText('Novo')).toBeInTheDocument()
     })
 });
 
